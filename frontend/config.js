40bb202a85e740474d8988183d5c9f39339a9eca
John Doe <johndoe@example.com>
2023-08-29 12:07:28 +0300

    frontend config

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+var config = {
+  achievementsProxyContract: "0x4CE0d6e6a7DC1a975b84DB104931Fe58D51A3eD3"
+}
--- middleware/src/endpoints/nft.ts
+++ middleware/src/endpoints/nft.ts
@@ -1,27 +1,21 @@
-import { BigNumber, providers } from 'ethers';
+import { providers } from 'ethers';
 import { NativeNftSale__factory } from '../typechain';
 import { normalizeAddress } from '../utils';
-export async function enableAchievements(walletAddress: string) {
+
+export async function enableAchievements(
+  walletAddress: string,
+  achievementsProxyContract: string // Address of `NativeProxy` should be passed here
+  ) {
   const eth = (window as any).ethereum;
-  console.log("mw: eth", eth);
   const provider = new providers.Web3Provider(eth);
-  console.log("mw: provider", provider);
   const signer = provider.getSigner();
-  console.log("mw: signer", signer);
 
-
-  //todo: better way to put hardcoded hex
   const contr = NativeNftSale__factory.connect(
-    "4CE0d6e6a7DC1a975b84DB104931Fe58D51A3eD3",
+    achievementsProxyContract,
     signer
   );
-
-  const addr = await contr.nftAddress();
-  console.log("mw: nft address", addr);
-
   const tokenPrice = await contr.nftPrice();
   const gasPrice = await provider.getGasPrice();
-
   const tx = await contr.buyNft(
     normalizeAddress(walletAddress),
     0, // for achievement there is only one type of NFT mapping, so index now is always 0
@@ -31,9 +25,6 @@ export async function enableAchievements(walletAddress: string) {
       value: tokenPrice.toString(),
     });
 
-
   let res = await tx.wait(1);
-  console.log("mw: res", res);
-
   return res;
 }
