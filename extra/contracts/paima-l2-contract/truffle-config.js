e896518ca2ac3d5658fd7aa4c306d14def2f5cf7
John Doe <johndoe@example.com>
2023-08-29 15:19:14 +0300

    extra files

---
+++ extra/contracts/paima-l2-contract/truffle-config.js
@@ -0,0 +1,33 @@
+const privateKey = process.env.PRIVATE_KEY;
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+module.exports = {
+  contracts_directory: "./src/contract",
+  contracts_build_directory: "./build",
+  migrations_directory: "./src/migrations",
+  compilers: {
+    solc: {
+      version: "0.8.13",
+      evmVersion: "berlin",
+    },
+  },
+  contract_config: {
+    owner: "",
+    fee: 10n ** 14n
+  },
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 7545,
+      network_id: "5777",
+    },
+    testnet: {
+      provider: () =>
+        new HDWalletProvider({
+          privateKeys: [privateKey],
+          providerOrUrl: "https://rpc-devnet-cardano-evm.c1.milkomeda.com",
+        }),
+      network_id: 200101,
+    },
+  },
+};
